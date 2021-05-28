33c5551c23ea3f755401fe04a97018757160f2fd
mwcomputer <kolbenfresser5@googlemail.com>
2021-05-28 12:04:18 +0200

    1

--- js/model.js
+++ js/model.js
@@ -1,5 +1,13 @@
 /* model (Daten/Logik) */
 
+let data = {
+    milk:{lower:0, upper:5, bev:"milch"},
+    juice:{lower:6, upper:12, bev:"saft"},
+    cola:{lower:13, upper:17, bev:"cola"},
+    wine:{lower:18, upper:120, bev:"wein"},
+    default:{bev:"tee"}
+}
 
-
-
+let gui =   {
+            img:{path:"./bilder/", ext:".jpg"} 
+        }
--- js/view_controller.js
+++ js/view_controller.js
@@ -2,9 +2,9 @@
 /* View- Controller */
 
 /* Der Plan
-    Einlesen Daten von Webseite :: 
+    Einlesen Daten von Webseite ::check 
 	Check Daten :: 
-    Btn. Trigger :: 
+    Btn. Trigger :: check
     Business-Logic (Alter --> Getränk) :: 
     Bild austauschen :: check!
 */
@@ -12,49 +12,67 @@
 // Modul Ablaufsteuerung | Test:
 controller()
 function controller() {
-    ausgabe(updateImg(checkAge(4)));
+    ausgabe(updateImg(checkAge(getInput())));
 }
 
 
 // Trigger - Btn 
+let btn = document.getElementById("trigBtn");
+btn.addEventListener("click",actOnClick);
 // Trigger - Input
 
 
+
 // Event-Dispatcher
+function actOnClick() {
+    if (isInputValid) {
+        controller()
+    } else {
+        ausgabe("input nicht korrekt.")
+        
+    }
+
+}
 
 // Check auf korrekte Eingaben ...
+function getInput(){
+    return false
+}
+
 
 // Modul Eingabe | Test:
-
-// Modul: Business-Logic (Mapping) | Test:
-// ausgabe(checkAge(2));
-// ausgabe(checkAge(6));
-// ausgabe(checkAge(15));
-// ausgabe(checkAge(20));
-// ausgabe(checkAge(132));
-// ausgabe(checkAge());
+//ausgabe(getInput());
+function getInput() {
+    let inputField = document.getElementsByName("eingabe")[0];
+    let age = parseInt(inputField.value);
+    return age;
+}
+//Modul: Business-Logic --> Test:
+/* ausgabe(checkAge(2));
+ausgabe(checkAge(6));
+ausgabe(checkAge(17));
+ausgabe(checkAge(20)); */
 function checkAge(age) {
-    switch (true) 
-    {
-        case (age >= 0) && (age <= 5):
-            return "milch";
-        case (age >= 6) && (age <= 12):
-            return "saft";
-        case (age >= 13) && (age <= 17):
-            return "cola";
-        case (age >= 18) && (age <= 130):
-            return "wein";    
-        default:
-            return "tee";  
-    }
+    switch (true) {
+        case (age >= data.milk.lower) && (age <= data.milk.upper):
+            return data.milk.bev;
+        case (age >= data.juice.lower) && (age <= data.juice.upper):
+            return data.juice.bev;
+        case (age >= data.cola.lower) && (age <= data.cola.upper):
+            return data.cola.bev;
+        case (age >= data.wine.lower) && (age <= data.wine.upper):
+            return data.wine.bev;
+                default:
+                return data.default.bev;   
+    } 
 }
 
 
 // Modul: Bild aktualisieren | Test:
-// ausgabe(updateImg("cola"));
+ ausgabe(updateImg("cola"));
 function updateImg(imgName) {
     let img = document.getElementById("bevImg");
-    img.src = "./bilder/" + imgName + ".jpg";
+    img.src = gui.img.path + imgName + gui.img.ext;
     return imgName; // monitoring
 }
 
